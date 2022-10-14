9c2e95f78131a994f2331da43cc3e56522ddc63f
LordSama <72462333+LordSama@users.noreply.github.com>
2022-10-14 15:50:07 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -5,57 +5,64 @@
 #include <iomanip>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-/*  Especificación:
-*       - Precondición P: { anyos > 0 && anyos < 100.000 }
-*       fun resolver(vector<string> const& ganadores, int anyos, string nacionalidad) dev int
-*       - Postcondición Q: { (max i: 0 <= i <= ganadores.size: nacionalidad == ganadores[i] ) && anyosSinGanar : (ganadores.size() + 1 - i) }
+/* Especificación:
+        - Precondición P: { n >= 0 ^ n < 150.000 ^ a > 0}
+        - Postcondición Q: { altos = for all i: 0 <= i < n: a > alt && bajos = for all j: 0 <= j < n: a <= alt }
+
+        Invariante  Inv: Para demostrar la corrección del bucle hacemos uso del predicado Invariante. En el bucle guardamos los pasajeros con
+        alturas menores o iguales a alt en el vector bajos y a los pasajeros con alturas mayores que alt en el vector altos.
+            Por tanto Inv = { (0 <= i < n) ^ (altos = for all i: 0 <= i < n: a > alt) ^ (bajos = for all j: 0 <= j < n: a <= alt) }
+
+        Función de cota: t(i) = v.size() - i.
+
+        Coste = O(n), siendo n el número de viajeros.
 */
-
-// función que resuelve el problema
-int resolver(vector<string> const& ganadores, int anyos, string nacionalidad) {
-    int anyosSinGanar;
-    int i = 0;
-  
-    while (i != anyos) {
-        if (ganadores[i] == nacionalidad) {
-
-        }
-        i++;
-    }
-
-    return anyosSinGanar;
-}
 
 // Resuelve un caso de prueba, leyendo de la entrada la
 // configuración, y escribiendo la respuesta
 bool resuelveCaso() {
     // leer los datos de la entrada
-    int anyos;                              // Número de años en que ha tenido lugar la competición.
-    string nacionalidad;                    // Nacionalidad en la que estamos interesados.
-    
-    cin >> anyos;
-    if (anyos == 0)
-        return false;
-    cin >> nacionalidad;
+    int n, a;                          // n es el número de viajeros y a es la altura máxima para considerar a alguien bajo.
 
-    vector<string> ganadores(anyos);        // Vector donde se almacenan los ganadores de cada edición, comenzando por la más antigua;
-    string aux;
-    for (int i = 0; i < anyos; i++) {
-        cin >> aux;
-        ganadores.push_back(aux);
+    cin >> n >> a;
+
+    if (!std::cin)
+        return false;
+
+    vector<string> altos;
+    vector<string> bajos;
+    string nombre;
+    int alt;
+
+    for (int i = 0; i < n; i++) {
+        cin >> nombre;
+        cin >> alt;
+        if (alt <= a) {
+            bajos.push_back(nombre);
+        }
+        else {
+            altos.push_back(nombre);
+        }
     }
 
-    int sol = resolver(ganadores, anyos, nacionalidad);
+    sort(bajos.begin(), bajos.end());
+    sort(altos.begin(), altos.end());
 
     // escribir sol
-    if (sol > 0) {
-        cout << sol << '\n';
+    cout << "Bajos: ";
+    for (int i = 0; i < bajos.size(); i++) {
+        cout << bajos[i] << " ";
     }
-    else {
-        cout << "NUNCA\n";
+    cout << '\n';
+
+    cout << "Altos: ";
+    for (int j = 0; j < altos.size(); j++) {
+        cout << altos[j] << " ";
     }
+    cout << '\n';
 
     return true;
 }
