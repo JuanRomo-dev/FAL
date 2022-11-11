54b8a3771916c7d323a6f5bd3d3195fd416e2802
LordSama <72462333+LordSama@users.noreply.github.com>
2022-11-11 10:32:06 +0100

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,66 @@
+﻿// Nombre del alumno Juan Romo Iribarren
+// Usuario del Juez F59
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+struct tSolucion{
+    int maximo;
+    int minimo;
+    bool
+};
+
+// función que resuelve el problema
+TipoSolucion resolver(TipoDatos datos) {
+
+
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+    // leer los datos de la entrada
+    vector<int> v;
+    int aux;
+
+    cin >> aux;
+    if (aux == 0)
+        return false;
+
+    while (aux != 0) {
+        v.push_back(aux);
+        cin >> aux;
+    }
+
+    TipoSolucion sol = resolver(datos);
+
+    // escribir sol
+
+    return true;
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    while (resuelveCaso())
+        ;
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
