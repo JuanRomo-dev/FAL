9f1e5bdbbafeb23ed9b0ca779397ff2fe1ccca9c
Juan <72462333+LordSama@users.noreply.github.com>
2023-01-04 11:31:13 +0100

    Update Source.cpp

--- 1_03 Número de máximos/Source.cpp
+++ 1_03 Número de máximos/Source.cpp
@@ -7,6 +7,23 @@
 #include <vector>
 using namespace std;
 
+/* 
+    ESPECIFICACIÓN:
+        Precondición PALG: {v.size()>0}
+        contMax(vector <int > v) dev {int maxi , int  cont}
+        Postcondición QALG: {(maxi == max k: 0 <= k < v.size() : v[k]) ∧ (cont == #k: 0 ≤ k < v.size() : v[k] == maxi)}
+        
+    INVARIANTE:
+        Inv: {(maxi == max k: 0 <= k < i : v[k]) ∧ (cont == #k : 0 <= k < i : v[k] == maxi) ∧ i <= v.size()}
+        
+    FUNCIÓN DE COTA: Esta es una función de las variables que intervienen en la condición del bucle que es positiva y decrece en cada vuelta del bucle.
+    Observando el bucle, vemos que la variable i crece en cada vuelta, por lo tanto -i decrece en cada vuelta. Para que la función sea positiva le sumamos 
+    un valor mayor que el máximo que pueda tomar la variable i. La función de cota elegida es:
+        t(i) = v.size() − i + 1
+        
+    COSTE: El coste del algoritmo es O(v.size()) lineal.
+*/
+
 void contMax(vector<int> const& v, int& maximo, int& cont) {
     maximo = v[0], cont = 1;
     int i = 1;
